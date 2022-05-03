09ef499630b7834e20b176176eecfaba71061018
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2022-05-03 21:41:56 +0800

    Add affine gap penalty implementation

--- align.py
+++ align.py
@@ -63,10 +63,64 @@ class DNAAlign:
         return self.alignment["dna1"] + "\n" + self.alignment["dna2"]
 
 
+class DNAAlignAffine:
+    def __init__(self, dna1, dna2, match=2, mismatch=-3, h=-1, s=-1) -> None:
+        self.dna1 = dna1
+        self.dna2 = dna2
+        self.score = {"match": match,
+                      "mismatch": mismatch,
+                      "gap_init": h,
+                      "gap_space": s}
+        self.m = len(dna1)
+        self.n = len(dna2)
+    
+        self.V = np.zeros((self.m + 1, self.n + 1))
+        self.E = np.zeros((self.m + 1, self.n + 1))
+        self.F = np.zeros((self.m + 1, self.n + 1))
+
+        self.alignment = {"dna1": "", "dna2": ""}
+    
+
+    def runDP(self):
+        # Initialize
+        self.E[0, 0] = float('-inf')
+        self.F[0, 0] = float('-inf')
+        for j in range(1, self.n + 1):
+            self.V[0, j] = self.score["gap_init"] + j * self.score["gap_space"]
+            self.E[0, j] = self.score["gap_init"] + j * self.score["gap_space"]
+            self.F[0, j] = float('-inf')
+        for i in range(1, self.m + 1):
+            self.V[i, 0] = self.score["gap_init"] + i * self.score["gap_space"]
+            self.E[i, 0] = float('-inf')
+            self.F[i, 0] = self.score["gap_init"] + i * self.score["gap_space"]
+        
+        
+        # DP
+        for i in range(1, self.m + 1):
+            for j in range(1, self.n + 1):
+                # Update E and F
+                self.E[i, j] = max(self.E[i, j-1] + self.score["gap_space"],
+                                   self.V[i, j-1] + self.score["gap_init"] + self.score["gap_space"])
+                self.F[i, j] = max(self.F[i-1, j] + self.score["gap_space"],
+                                   self.V[i-1, j] + self.score["gap_init"] + self.score["gap_space"])
+                
+                delta = self.score["match"] if self.dna1[i-1] == self.dna2[j-1] else self.score["mismatch"]
+                self.V[i, j] = max(self.V[i-1, j-1] + delta, self.E[i, j], self.F[i, j])
+        
+        return np.max(self.V)
+
+
+
 if __name__ == "__main__":
     prob = DNAAlign("ACTCGATC", "GTTCGCCT")
     print(prob.runDP())
     print(prob.D)
     print(prob)
+
+    prob = DNAAlignAffine("ACCGA", "AGTTA", 1, -3, -1, -1)
+    prob.runDP()
+    print(prob.V)
+    print(prob.E)
+    print(prob.F)
 
     
